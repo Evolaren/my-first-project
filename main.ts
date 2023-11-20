96aab8cd72d8239bd2c7ddbba0839f750e082136
Evolaren <151584887+Evolaren@users.noreply.github.com>
2023-11-21 10:23:44 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+basic.showLeds(`
+    # # # # #
+    # . . . .
+    # # # # #
+    . . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # # # # .
+    # . . . #
+    # # # # #
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.showIcon(IconNames.Happy)
